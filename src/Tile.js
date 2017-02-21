b640428ab9f03c867bd06d72c673898e66a006c3
Richard Lee <rblee88@gmail.com>
2017-02-21 17:36:39 +1100

    Add shell for tiling

---
+++ src/Tile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Tile.css';
+
+class Tile extends Component {
+  render() {
+    return (
+      <div>Testing</div>
+    )
+  }
+}
+
+export default Tile;
